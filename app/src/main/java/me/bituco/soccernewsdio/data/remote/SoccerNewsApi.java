d6588e86daa1baa9170de590897fbb00889b793d
fernandobituco <fernandoangelima@gmail.com>
2022-06-27 13:00:16 -0300

    adapter loading image and oppening link implemented

---
+++ app/src/main/java/me/bituco/soccernewsdio/data/remote/SoccerNewsApi.java
@@ -0,0 +1,13 @@
+package me.bituco.soccernewsdio.data.remote;
+
+import java.util.List;
+
+import me.bituco.soccernewsdio.domain.News;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface SoccerNewsApi {
+
+    @GET("news.json")
+    Call<List<News>> getNews();
+}
--- app/src/main/java/me/bituco/soccernewsdio/ui/news/NewsViewModel.java
+++ app/src/main/java/me/bituco/soccernewsdio/ui/news/NewsViewModel.java
@@ -1,29 +1,56 @@
 package me.bituco.soccernewsdio.ui.news;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.bituco.soccernewsdio.data.remote.SoccerNewsApi;
 import me.bituco.soccernewsdio.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
-
-        List<News> news = new ArrayList<>();
-        news.add(new News("jogo decisivo", "jogo muito importante"));
-        news.add(new News("jogo não decisivo", "jogo nem tão importante"));
-        news.add(new News("jogo nada decisivo", "jogo nem um pouco importante"));
-
-        this.news.setValue(news);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://fernandobituco.github.io/Soccer-News-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
     }
 
-    public MutableLiveData<List<News>> getNews() {
-        return news;
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
+                if (response.isSuccessful()) {
+                    news.setValue(response.body());
+                } else {
+                    Log.d("DEBUG", "Response not successful");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<News>> call, @NonNull Throwable t) {
+                Log.d("DEBUG", t.toString());
+            }
+        });
+    }
+
+    public LiveData<List<News>> getNews() {
+        return this.news;
     }
 }
